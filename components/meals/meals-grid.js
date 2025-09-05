d28e6f0c9fdb917f62e213c55912ee9fd6b33e71
Oleh Shpoltakov <ggsubsy@gmail.com>
2025-09-05 18:44:12 +0200

    feat(meals): add client-side pagination to MealsGrid with MealsPagination

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -1,13 +1,34 @@
+"use client";
+
+import { useState } from "react";
 import MealItem from "./meal-item";
+import MealsPagination from "./meals-pagination";
 
 export default function MealsGrid({ meals }) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 8;
+  const totalPages = Math.ceil(meals.length / itemsPerPage);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const currentMeals = meals.slice(startIndex, startIndex + itemsPerPage);
+
   return (
-    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 list-none p-0 m-0">
-      {meals.map((meal) => (
-        <li key={meal.id}>
-          <MealItem {...meal} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <div className="w-full max-w-[1440px] mx-auto">
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 list-none p-0 m-0">
+          {currentMeals.map((meal) => (
+            <li key={meal.id}>
+              <MealItem {...meal} />
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <MealsPagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={setCurrentPage}
+      />
+    </>
   );
 }
