247222904901edd68cbd9e62719d37af4d4e3b30
Oleh Shpoltakov <ggsubsy@gmail.com>
2025-09-06 17:53:40 +0200

    feat(ui): create ImagePicker component with preview

---
+++ components/ui/image-picker.js
@@ -0,0 +1,67 @@
+import { useState, useRef } from "react";
+import Image from "next/image";
+
+import { Label } from "./label";
+import { Input } from "./input";
+import { Button } from "./button";
+
+export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState(null);
+  const pickerRef = useRef();
+
+  function handlePickClick() {
+    pickerRef.current.click();
+  }
+
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(file);
+  }
+
+  return (
+    <div className="flex flex-col gap-2">
+      <Label htmlFor={name}>{label}</Label>
+
+      <div className="grid grid-cols-2 gap-4">
+        <div className="w-full h-40 border rounded-md flex items-center justify-center overflow-hidden relative">
+          {pickedImage ? (
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user."
+              fill
+              className="object-cover object-center"
+            />
+          ) : (
+            <span className=" text-sm text-gray-400">No image picked yet</span>
+          )}
+          <Input
+            id={name}
+            name={name}
+            type="file"
+            accept="image/png, image/jpeg"
+            ref={pickerRef}
+            onChange={handleImageChange}
+            className="hidden"
+          />
+        </div>
+        <div className="flex flex-col justify-end">
+          <Button type="button" onClick={handlePickClick} className="w-full">
+            Pick an Image
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
