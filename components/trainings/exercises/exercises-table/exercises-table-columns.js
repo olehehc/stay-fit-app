dc5c24c6aa076161e1134201525e04143e5c46c2
Oleh Shpoltakov <ggsubsy@gmail.com>
2025-09-16 18:53:08 +0200

    feat(exercises): add table column config for exercises

---
+++ components/trainings/exercises/exercises-table/exercises-table-columns.js
@@ -0,0 +1,14 @@
+export const ExercisesTableColumns = [
+  {
+    accessorKey: "title",
+    header: "Title",
+  },
+  {
+    accessorKey: "muscle_group",
+    header: "Muscle Group",
+  },
+  {
+    accessorKey: "exercise_type",
+    header: "Exercise Type",
+  },
+];
