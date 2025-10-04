a2e31811b02a7f921d174dcfc2b27d3190c6552f
Oleh Shpoltakov <ggsubsy@gmail.com>
2025-10-04 19:53:22 +0200

    feat(trainings): add TrainingItem component with actions

---
+++ components/trainings/training-item.js
@@ -0,0 +1,35 @@
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Button } from "../ui/button";
+
+export default function TrainingItem({
+  trainingId,
+  title,
+  trainingDate,
+  onDelete,
+}) {
+  return (
+    <div className="bg-white w-full rounded-md border shadow-sm p-4 flex flex-row items-center justify-between w-xl">
+      <div>
+        <h2 className="text-lg font-semibold">{title}</h2>
+        <p className="text-sm text-gray-500">📅 {trainingDate}</p>
+      </div>
+      <div className="flex gap-2">
+        <Button title="Start training" variant="outline">
+          <PlayArrowIcon />
+        </Button>
+        <Button title="Edit training" variant="outline">
+          <EditIcon />
+        </Button>
+        <Button
+          title="Delete training"
+          variant="destructive"
+          onClick={() => onDelete(trainingId)}
+        >
+          <DeleteIcon />
+        </Button>
+      </div>
+    </div>
+  );
+}
