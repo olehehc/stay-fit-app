b72c7abee8d608b56e86ef901bc06194497bb195
Oleh Shpoltakov <ggsubsy@gmail.com>
2025-10-26 14:43:56 +0100

    feat(session-page): create ExercisesList component to render list of ExerciseItem components

---
+++ components/sessions/exercises/exercises-list.js
@@ -0,0 +1,24 @@
+"use client";
+
+import ExerciseItem from "./exercise-item";
+
+export default function ExercisesList({ training, toggleComplete, updateSet }) {
+  return (
+    <ul className="space-y-4">
+      {training.exercises.map((exercise) => (
+        <ExerciseItem
+          key={exercise.id}
+          title={exercise.title}
+          exerciseType={exercise.exercise_type}
+          muscleGroup={exercise.muscle_group}
+          instructions={exercise.instructions}
+          image={exercise.image}
+          exerciseId={exercise.id}
+          sets={exercise.sets}
+          toggleComplete={toggleComplete}
+          updateSet={updateSet}
+        />
+      ))}
+    </ul>
+  );
+}
