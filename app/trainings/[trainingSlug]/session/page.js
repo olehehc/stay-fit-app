144ddfdc6170eba5681d2d91fc57736458e50fb6
Oleh Shpoltakov <ggsubsy@gmail.com>
2025-10-30 13:14:59 +0100

    refactor(session): use slug-based fetching in TrainingSessionPage

--- app/trainings/[trainingSlug]/session/page.js
+++ app/trainings/[trainingSlug]/session/page.js
@@ -1,22 +1,20 @@
 import TrainingSession from "@/components/sessions/trainings/training-session";
 import TrainingSessionResults from "@/components/sessions/trainings/training-session-results";
 import { getCurrentUser } from "@/lib/auth";
-import { getTrainingSessionByUserAndTrainingId } from "@/lib/repository/sessions";
-import { getTrainingAndTrainingSetsByUserAndTrainingId } from "@/lib/repository/trainings";
+import { getTrainingAndTrainingSetsBySlug } from "@/lib/repository/trainings";
+import { getTrainingSessionBySlug } from "@/lib/repository/sessions";
 
-export default async function TrainingSessionPage({ searchParams }) {
-  const query = await searchParams;
-  const trainingIdStr = query?.trainingId;
-  const trainingId = trainingIdStr ? parseInt(trainingIdStr, 10) : null;
+export default async function TrainingSessionPage({ params }) {
+  const { trainingSlug } = await params;
 
-  if (!trainingId) {
-    throw new Error("Invalid training ID");
+  if (!trainingSlug) {
+    throw new Error("Invalid training slug");
   }
 
   const user = await getCurrentUser();
 
-  const trainingData = getTrainingAndTrainingSetsByUserAndTrainingId(
-    trainingId,
+  const trainingData = await getTrainingAndTrainingSetsBySlug(
+    trainingSlug,
     user.id
   );
 
@@ -27,10 +25,7 @@ export default async function TrainingSessionPage({ searchParams }) {
   let trainingSessionData;
 
   if (trainingData.completed) {
-    trainingSessionData = await getTrainingSessionByUserAndTrainingId(
-      trainingId,
-      user.id
-    );
+    trainingSessionData = await getTrainingSessionBySlug(trainingSlug, user.id);
   }
 
   return (
