a0d999b425814043519098e4c40ec7f7f2961a43
Oleh Shpoltakov <ggsubsy@gmail.com>
2025-11-09 20:38:05 +0100

    refactor(error): move error boundary to root and simplify component

--- app/error.js
+++ app/error.js
@@ -1,9 +1,9 @@
 "use client";
 
-export default function Error({ error, reset }) {
+export default function Error({ error }) {
   return (
     <div className="pt-[92px] p-6 text-center">
-      <h2 className="text-xl font-semibold mb-4">Something went wrong 😕</h2>
+      <h1 className="text-xl font-semibold mb-4">An error occurred!</h1>
       <p className="mb-6 text-gray-600">{error.message}</p>
     </div>
   );
