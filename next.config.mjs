4faeb25426571d91d0d8bb034ce44f8a51f147f2
Oleh Shpoltakov <ggsubsy@gmail.com>
2025-11-10 10:55:28 +0100

    chore(next): increase server action body size limit to 15mb and configure remote S3 image domain

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,18 @@
 const nextConfig = {
   experimental: {
     serverActions: {
-      bodySizeLimit: "5mb",
+      bodySizeLimit: "15mb",
     },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s3.eu-north-1.amazonaws.com",
+        port: "",
+        pathname: `/${process.env.NEXT_PUBLIC_AWS_IMAGE_HOSTNAME}/**`,
+      },
+    ],
   },
 };
 
