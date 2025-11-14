8fa6bb020a65694967bdb0a75285e1f09abc81a6
Oleh Shpoltakov <ggsubsy@gmail.com>
2025-11-14 22:09:36 +0100

    refactor(exercises): allow custom className prop in ExercisesTableRow

--- components/trainings/exercises/exercises-table/exercises-table-row.js
+++ components/trainings/exercises/exercises-table/exercises-table-row.js
@@ -3,7 +3,7 @@ import { useDraggable } from "@dnd-kit/core";
 import { TableRow, TableCell } from "@/components/ui/table";
 import { flexRender } from "@tanstack/react-table";
 
-export default function ExercisesTableRow({ row }) {
+export default function ExercisesTableRow({ row, className }) {
   const { attributes, listeners, setNodeRef, isDragging } = useDraggable({
     id: row.original.id.toString(),
     data: { row },
@@ -15,7 +15,9 @@ export default function ExercisesTableRow({ row }) {
       data-row-id={row.original.id.toString()}
       {...attributes}
       {...listeners}
-      className={`cursor-grab select-none ${isDragging ? "opacity-50" : ""}`}
+      className={`cursor-grab select-none ${
+        isDragging ? "opacity-50" : ""
+      } ${className}`}
     >
       {row.getVisibleCells().map((cell) => (
         <TableCell
