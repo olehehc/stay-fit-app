5d2dd11ad39187851e25964c2b0c8410db568b76
Oleh Shpoltakov <ggsubsy@gmail.com>
2025-11-16 16:08:55 +0100

    refactor(header): center header content and constrain width for large screens

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -7,15 +7,17 @@ export default async function MainHeader() {
   const user = await getCurrentUser();
 
   return (
-    <header className="flex p-4 fixed top-0 left-0 w-full z-50 bg-black justify-between items-center">
-      <Link href="/" className="text-white text-xl font-bold">
-        💪 StayFit
-      </Link>
-      <div className="hidden md:flex">
-        <Navigation user={user} />
-      </div>
-      <div className="md:hidden">
-        <MobileNavigation user={user} />
+    <header className="fixed top-0 left-0 w-full z-50 bg-black justify-between items-center">
+      <div className="flex max-w-[1440px] mx-auto p-4 justify-between items-center">
+        <Link href="/" className="text-white text-xl font-bold">
+          💪 StayFit
+        </Link>
+        <div className="hidden md:flex">
+          <Navigation user={user} />
+        </div>
+        <div className="md:hidden">
+          <MobileNavigation user={user} />
+        </div>
       </div>
     </header>
   );
